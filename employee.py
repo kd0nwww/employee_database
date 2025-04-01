aad5e16186b707ef936f63702765d49605fc19d7
Kirill Donetskov <kirilldonetskov@gmail.com>
2025-04-01 21:25:05 +0600

    Fixed errors

--- employee.py
+++ employee.py
@@ -6,8 +6,35 @@ class Employee:
         self.salary = salary
         self.hire_date = hire_date
 
+    def get_id(self):
+        return self.id
+    
+    def get_name(self):
+        return self.name
+    
+    def get_position(self):
+        return self.position
+    
+    def get_salary(self):
+        return self.salary
+    
+    def get_hire_date(self):
+        return self.hire_date
+    
+    def set_id(self, id):
+        self.id = id
+
+    def set_name(self, name):
+        self.name = name
+
+    def set_position(self, position):
+        self.position = position
+
+    def set_salary(self, salary):
+        self.salary = salary
+
+    def set_hire_date(self, hire_date):
+        self.hire_date = hire_date
+
     def __str__(self):
         return f"Employee ID: {self.id}, Name: {self.name}, Position: {self.position}, Salary: {self.salary}, Hire Date: {self.hire_date}"
-    
-emp = Employee(1, "John Doe", "Software Engineer", 70000, "2023-01-01")
-print(emp)
