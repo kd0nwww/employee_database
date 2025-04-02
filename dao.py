15ca8ab9d9f44d381aad1a00a2b8ccf83b858e86
Kirill Donetskov <kirilldonetskov@gmail.com>
2025-04-02 15:05:24 +0600

    Updated DAO

--- dao.py
+++ dao.py
@@ -12,3 +12,35 @@ class EmployeeDAO:
 
         self.__connection.commit()
         return self.__cursor.lastrowid
+    
+    def get_by_id(self, id):
+        sql = '''SELECT * FROM employee WHERE id = ?'''
+        self.__cursor.execute(sql, (id,))
+        row = self.__cursor.fetchone()
+        if row:
+            return Employee(row[0], row[1], row[2], row[3], row[4])
+        return None
+    
+    def get_all(self):
+        sql = '''SELECT * FROM employee'''
+        self.__cursor.execute(sql)
+        rows = self.__cursor.fetchall()
+        employees = []
+        for row in rows:
+            employees.append(Employee(row[0], row[1], row[2], row[3], row[4]))
+        return employees
+    
+    def update(self, employee: Employee):
+        sql = '''UPDATE employee SET name = ?, position = ?, salary = ?, hire_date = ? WHERE id = ?'''
+        self.__cursor.execute(sql, (employee.get_name(), employee.get_position(), employee.get_salary(), employee.get_hire_date(), employee.get_id()))
+        self.__connection.commit()
+
+        return self.__cursor.rowcount
+    
+    def delete(self, id):
+        sql = '''DELETE FROM employee WHERE id = ?'''
+        self.__cursor.execute(sql, (id,))
+
+        self.__connection.commit()
+
+        return self.__cursor.rowcount
